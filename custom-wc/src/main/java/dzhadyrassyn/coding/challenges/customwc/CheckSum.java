7da69e20016934cc87c67e80361a6d02651aed2f
Daniyar Zhadyrassyn <daniyar.zhadyrassyn@agile-track.com>
2024-03-06 13:18:21 +0600

    Experimental application. Checksum

---
+++ custom-wc/src/main/java/dzhadyrassyn/coding/challenges/customwc/CheckSum.java
@@ -0,0 +1,35 @@
+package dzhadyrassyn.coding.challenges.customwc;
+
+import picocli.CommandLine;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.*;
+
+@Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
+    description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
+public class CheckSum implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private File file;
+
+    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+    private String algorithm = "SHA-256";
+
+    @Override
+    public Integer call() throws Exception {
+        byte[] fileContents = Files.readAllBytes(file.toPath());
+        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+        System.out.printf("%0" + (digest.length * 2) + "x%n", new BigInteger(1, digest));
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new CheckSum()).execute(args);
+        System.exit(exitCode);
+    }
+}
